9e6e34cac2079321dd06ab9f35ba974f5c797f4c
Junior Miksza <junior.miksza8@gmail.com>
2022-07-04 22:56:28 -0300

    set minimum test coverage and crypt hash test

--- src/adapters/BcryptHashService.ts
+++ src/adapters/BcryptHashService.ts
@@ -2,7 +2,7 @@ import { HashService } from '../ports/HashService'
 import { genSalt, hash as BcryptHash } from 'bcrypt'
 
 export class BcryptHashService implements HashService {
-  saltRounds = 10
+  private saltRounds = 10
 
   async hash(text: string) {
     const salt = await genSalt(this.saltRounds)
